6d30d4dab9cbccf21e108593a5133ff1379164ea
rupps9800 <rupps9800@gmail.com>
2013-07-10 22:07:23 -0700

    index reading

--- web.js
+++ web.js
@@ -1,9 +1,10 @@
 var express = require('express');
-
+var fs = require('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello Kyle!');
+  var buff=fs.readFileSync("index.html"); 
+  response.send(buff.toString());
 });
 
 var port = process.env.PORT || 5000;
